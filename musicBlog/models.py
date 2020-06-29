955634a8cb32032af6e8d64846fe2ec998022715
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-29 09:19:20 +0530

    User and Post model definitions for Database

---
+++ musicBlog/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from musicBlog import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref=db.backref('author', lazy=True))
+    def __repr__(self):
+        return f"User({self.username}, {self.email})"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __repr__(self):
+        return f"Post({self.title}, {self.date_posted})"
+    
