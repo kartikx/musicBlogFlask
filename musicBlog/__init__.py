719a594072bf81a4afba4a8c41856cdad090be1f
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-29 09:20:20 +0530

    Add login_manager and bcrypt instantiation to init.py

--- musicBlog/__init__.py
+++ musicBlog/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "57999d74c5d3a47c13fdb4345efe3e6a"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Please login to view this page"
+login_manager.login_message_category = "info"
 
 # ? Important to be kept at the end.
 from musicBlog import routes
